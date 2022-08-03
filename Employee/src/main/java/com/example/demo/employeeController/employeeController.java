0161c11ed845f63ad86736d3d0b900d9b0a05d03
Hima <Hima@DESKTOP-8U7O14D>
2022-08-03 17:06:55 +0530

    testing

---
+++ Employee/src/main/java/com/example/demo/employeeController/employeeController.java
@@ -0,0 +1,25 @@
+package com.example.demo.employeeController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.employeeEntity.Employee;
+import com.example.demo.employeeService.employeeService;
+
+@RestController
+@RequestMapping("/demo")
+public class employeeController {
+
+	
+@Autowired
+public employeeService empService;
+
+@PostMapping("/save")
+public void saveEmploye(@RequestBody Employee employee) {
+	empService.saveEmploy(employee);
+}
+
+}
